f3f23936740e0659512aa74e4dd2df4aa402a215
hanamaru <53654878+H4n4m4ru@users.noreply.github.com>
2020-07-17 20:36:59 +0800

    Add files via upload

---
+++ DA_Pjct_02/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace DA_Pjct_02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::DA_Pjct_02.Properties.Resources._5845cd230b2a3b54fdbaecf7;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 567);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(47)))), ((int)(((byte)(47)))));
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseLeave += new System.EventHandler(this.Form1_MouseLeave);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ DA_Pjct_02/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DA_Pjct_02
+{
+    public partial class Form1 : Form
+    {
+        //To control form's moving
+        private bool b_FormIsMoving = false;
+        private int i_OrgFormLoct_X = 0, i_OrgFormLoct_Y = 0, i_OrgMouseLoct_X = 0, i_OrgMouseLoct_Y = 0;
+        //
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+
+        //To control form's moving
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            b_FormIsMoving = true;
+            i_OrgFormLoct_X = this.Location.X;
+            i_OrgFormLoct_Y = this.Location.Y;
+            i_OrgMouseLoct_X = MousePosition.X;
+            i_OrgMouseLoct_Y = MousePosition.Y;
+        }
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            b_FormIsMoving = false;
+        }
+        private void Form1_MouseLeave(object sender, EventArgs e)
+        {
+            b_FormIsMoving = false;
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (b_FormIsMoving) {
+                this.SetDesktopLocation(i_OrgFormLoct_X + MousePosition.X - i_OrgMouseLoct_X, i_OrgFormLoct_Y + MousePosition.Y - i_OrgMouseLoct_Y);
+            }
+        }
+
+        //Not important stuffs
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            using (Graphics g = this.CreateGraphics())
+            {
+                g.DrawRectangle(new Pen(Color.FromArgb(74, 74, 74), 1), new Rectangle(1, 1, 798, 598));
+            }
+        }
+    }
+}
